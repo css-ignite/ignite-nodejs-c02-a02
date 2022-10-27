95bd4a483447e5763a5eb8973f418d02d290d2ff
Claudney Sessa <claudneysartisessa@gmail.com>
2022-10-26 21:35:25 -0300

    Ajustando o nome do arquivo Category.ts pois estava em minúsculo

--- src/repositories/ICategoriesRepository.ts
+++ src/repositories/ICategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from "../models/category";
+import { Category } from "../models/Category";
 
 /*
     Interface para tipar o objeto que será passado como parâmetro para o método execute
