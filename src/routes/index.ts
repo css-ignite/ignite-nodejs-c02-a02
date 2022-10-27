5a97e18ae3090a2a54a188a86ddad4278862b1df
claudneysessa <claudneysartisessa@gmail.com>
2022-10-27 05:03:51 -0300

    Adicionando a auto documentação com o swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,128 @@ import { specificationsRoutes } from "./spacifications.routes";
 
 const router = Router();
 
+/**
+ * @openapi
+ * /categories:
+ *   post:
+ *     tags:
+ *       - categories
+ *     summary: categories
+ *     description: Adicionar uma categoria
+ *     operationId: addCategory
+ *     requestBody:
+ *       description: Criar uma categoria
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nome da Categoria.
+ *                 example: Nome da Categoria
+ *               description:
+ *                 type: string
+ *                 description: Descrição da Categoria.
+ *                 example: Descrição da Categoria
+ *       required: true
+ *     responses:
+ *      '200':
+ *        description: Successful operation
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Pet'
+ *      '405':
+ *        description: Invalid input
+ *   get:
+ *     tags:
+ *       - categories
+ *     summary: categories
+ *     description: Listar categorias
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ * /categories/{name}:
+ *   get:
+ *     tags:
+ *       - categories
+ *     summary: categories
+ *     description: Listar categoria pelo nome
+ *     operationId: getCategoriesByName
+ *     parameters:
+ *       - name: name
+ *         in: path
+ *         description: Nome da categoria
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 router.use("/categories", categoriesRoutes);
+
+/**
+ * @openapi
+ * /specifications:
+ *   post:
+ *     tags:
+ *       - specifications
+ *     summary: specifications
+ *     description: Adicionar uma specifications
+ *     operationId: addspecifications
+ *     requestBody:
+ *       description: Criar uma specifications
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nome da specifications.
+ *                 example: Nome da specifications
+ *               description:
+ *                 type: string
+ *                 description: Descrição da specifications.
+ *                 example: Descrição da specifications
+ *       required: true
+ *     responses:
+ *      '200':
+ *        description: Successful operation
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Pet'
+ *      '405':
+ *        description: Invalid input
+ *   get:
+ *     tags:
+ *       - specifications
+ *     summary: specifications
+ *     description: Listar specifications
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ * /specifications/{name}:
+ *   get:
+ *     tags:
+ *       - specifications
+ *     summary: specifications
+ *     description: Listar specification pelo nome
+ *     operationId: getPetById
+ *     parameters:
+ *       - name: name
+ *         in: path
+ *         description: Nome da specification
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Retorna uma specifications.
+ */
 router.use("/specifications", specificationsRoutes);
 
 export { router };
